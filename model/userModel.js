f8f022915b536d3f70e1c0a7acb566802b86f300
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-27 09:15:18 +0530

    static functions created for register and login logic

--- model/userModel.js
+++ model/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = mongoose.Schema(
 	{
@@ -8,6 +9,25 @@ const UserSchema = mongoose.Schema(
 	},
 	{ timestamps: true },
 );
+
+UserSchema.statics.signup = async function (password) {
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(password, salt);
+	return hash;
+};
+
+UserSchema.statics.login = async function (email, password) {
+	const user = await this.findOne({ email });
+	if (!user) {
+		throw new Error('User not found');
+	}
+
+	const doesPasswordMatch = await bcrypt.compare(password, user.password);
+	if (!doesPasswordMatch) {
+		throw new Error('Password does not match');
+	}
+	return user;
+};
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
