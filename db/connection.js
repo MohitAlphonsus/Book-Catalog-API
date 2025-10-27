4ca0b32947fe5c8cb46cf349118d159dd8b80f36
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-27 11:42:20 +0530

    mongoDB connection fixed for render

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 async function connectToDatabase() {
 	try {
-		await mongoose.connect(process.env.MONGODB_URI);
+		await mongoose.connect(process.env.MONGODB_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		console.log('Connected to MongoDB');
 	} catch (error) {
 		console.error('Error connecting to MongoDB:', error);
+		throw error;
 	}
 }
 
